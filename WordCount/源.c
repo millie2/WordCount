4923d76625fdd4b9efa16c2de0101ef7a9892a4d
millie2 <389297707@qq.com>
2020-05-20 02:27:33 +0800

    排版：注释调整

--- WordCount/源.c
+++ WordCount/源.c
@@ -25,40 +25,38 @@ int main()
 	int word=0;
 
 	gets(parameter);
-	gets(filename);
-	//获取输入的两个字符串
+	gets(filename);                    //获取输入的两个字符串
 
 	fp=fopen(filename,"r");
 	if(fp==NULL)
 	{
-		printf("文件打开失败\n");
+		printf("文件打开失败\n");      //判断文件是否正常打开，及时反馈
 		return ;
 	}
-	//判断文件是否正常打开，及时反馈
-
+	                           
 	if ( parameter[1]=='c')
 	{
-		while(!feof(fp))   //判断文件是否结束
+		while(!feof(fp))               //判断文件是否结束
 		{
-			fgetc(fp)&&character++; 
+			fgetc(fp)&&character++;    //如获取字符成功则character计数加一
 		}
-		//如获取字符成功则character计数加一
+		
 
 		printf("字符数: %d\n",character);
 	}
-	else                    //当输入-w时
+	else                               //当输入-w时
 	{
-		while(!feof(fp))   //判断文件是否结束
+		while(!feof(fp))              //判断文件是否结束
 		{
 			char middle=0;
-			middle=fgetc(fp);
-			//设置中间变量middle用于缓存取出的字符
+			middle=fgetc(fp);        //设置中间变量middle用于缓存取出的字符
+			
 
 			if ((middle==' ')||(middle==',')||(middle=='.'))
 			{
-				word++;
+				word++;             //当字符为空格、顿号和句号时word计数器加一
 			} 
-			//当字符为空格、顿号和句号时word计数器加一
+			
 		}
 
 		printf(" 单词数: %d\n",word);
